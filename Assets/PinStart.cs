c534d39e1f34fabfdd792841ef900c0bbf3d11aa
Nikk <nikkigazejr@gmail.com>
2021-10-28 12:35:06 +0300

    added pinball and chain

---
+++ Assets/PinStart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PinStart : MonoBehaviour
+{
+    [SerializeField] private float _cooldown;
+
+    private float _lastTime;
+    private void Start()
+    {
+        _lastTime = 0;
+    }
+
+    private void Update()
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.forward * 30000);
+        // if (Time.time - _lastTime >= _cooldown)
+        // {
+        //     // transform.position = transform.position - new Vector3(0, 0, 0.5f);
+        //     GetComponent<Rigidbody>().AddForce(Vector3.forward * 10000);
+        //     _lastTime = Time.time;
+        //     Debug.Log("St");
+        // }
+    }
+}
